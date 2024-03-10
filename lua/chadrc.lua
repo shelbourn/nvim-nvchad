edc8f700f2026ffe9688f3bc77ac6db953c52142
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-10 03:13:08 -0500

    Updating config with syntax hightlighting modules

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,7 +1,34 @@
 local M = {}
 
 M.ui = {
+
+  -- themeing
   theme = "onedark",
+
+  -- lsp configs
+
+  -- syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- web dev 
+        "css",
+        "html",
+        "javascript",
+        "json",
+        "php",
+        "python",
+        "tsx",
+        "typescript",
+
+       -- low level
+        "c",
+        "rust",
+        "zig",
+      },
+    },
+  },
 }
 
 return M
