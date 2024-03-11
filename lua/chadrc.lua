1e2137753465d38d97bfa9144db057fec4881786
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-10 21:49:42 -0500

    Updating before forking the main repo

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,41 +1,8 @@
 local M = {}
 
 M.ui = {
-
 	-- themeing
 	theme = "onedark",
-
-	-- lsp configs
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("nvchad.configs.lspconfig").defaults()
-			require("configs.lspconfig")
-		end,
-	},
-
-	-- syntax highlighting
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				-- web dev
-				"css",
-				"html",
-				"javascript",
-				"json",
-				"php",
-				"python",
-				"tsx",
-				"typescript",
-
-				-- low level
-				"c",
-				"rust",
-				"zig",
-			},
-		},
-	},
 }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,47 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    config = function()
-      require "configs.conform"
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("configs.conform")
+		end,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      git = { enable = true },
-    },
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			git = { enable = true },
+		},
+	},
+
+	-- lsp configs
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require("configs.lspconfig")
+		end,
+	},
+
+	-- syntax highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				-- web dev
+				"css",
+				"html",
+				"javascript",
+				"json",
+				"php",
+				"python",
+				"tsx",
+				"typescript",
+
+				-- low level
+				"c",
+				"rust",
+				"zig",
+			},
+		},
+	},
 }
